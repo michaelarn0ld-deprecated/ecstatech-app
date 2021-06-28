4adb7e90488b916fd2f686e3efe3fbe10acc07d0
Michael Arnold <marnolddevelop@gmail.com>
2021-06-27 21:20:12 -0700

    ipad viewing

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,54 @@
 import React from "react"
 import "../styles/Services.scss"
+import webdev from "../images/webdev.svg"
 
 const Services = () => {
   return (
     <div className="services">
-      <div className="another"></div>
+      <div className="services-text">
+        <h1>How can we help you?</h1>
+        <p>Here's a list of the services we offer</p>
+      </div>
+      <div className="services-glass">
+        <div className="services-glass-item one">
+          <div></div>
+          <h2>Web Development</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            nostrum eligendi dolorem?
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="services-glass-item two">
+          <div></div>
+          <h2>Mobile Apps</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            nostrum eligendi dolorem?
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="services-glass-item three">
+          <div></div>
+          <h2>Data Analytics</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            nostrum eligendi dolorem?
+          </p>
+          <button>Learn More</button>
+        </div>
+        <div className="services-glass-item four">
+          <div></div>
+          <h2>Automation</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            nostrum eligendi dolorem?
+          </p>
+          <button>Learn More</button>
+        </div>
+      </div>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
     </div>
   )
 }
