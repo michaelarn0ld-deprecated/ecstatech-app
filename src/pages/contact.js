a89f3cbd50d178e820877de3762dd9ea80054c8f
Michael Arnold <marnolddevelop@gmail.com>
2021-07-13 11:58:29 -0700

    gsap tween to contact page on render

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,26 @@
 import React from "react"
+import { useEffect } from "react"
+import { gsap } from "gsap"
 import ContactPage from "../components/ContactPage"
 
 const Contact = () => {
+  useEffect(() => {
+    gsap.to(".contact-page > header", {
+      opacity: 1,
+      y: 0,
+      duration: 0.6,
+      delay: 0.1,
+      ease: "back.out(2)",
+    })
+    gsap.to(".contact-page > main", {
+      opacity: 1,
+      y: 0,
+      duration: 0.6,
+      delay: 0.1,
+      ease: "back.out(2)",
+    })
+  }, [])
+
   return <ContactPage />
 }
 
